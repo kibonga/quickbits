ad7224b25a0deda80b7c1c9b5b6efce90a66e7b7
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-07-19 22:46:41 +0200

    added serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,11 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	staticFileServer := http.FileServer(http.Dir("../../ui/static"))
+	staticFileHandler := http.StripPrefix("/static", staticFileServer)
+
+	mux.Handle("/static/", staticFileHandler)
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
