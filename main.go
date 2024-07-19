26d8edca316cd3a777b7bf701837d51feb38a6a3
Pavle Djurdjic <66330623+kibonga@users.noreply.github.com>
2024-07-19 14:31:47 +0200

    finished customizing headers example (#1)
    Co-authored-by: Pavle Durdic <pavle.durdic.ext@infront.co>

--- main.go
+++ main.go
@@ -14,12 +14,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from quickbits home!"))
 }
 
+func content(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name":"Pavle"}`))
+}
+
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.Header().Set("Kibonga", "Kimur")
+		// w.WriteHeader(http.StatusMethodNotAllowed)
+		// w.Write([]byte("Method not allowed\n"))
+		http.Error(w, "Method not allowed(http)\n", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet...\n"))
 }
 
 func main() {
@@ -27,6 +41,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/content", content)
 
 	// Potential security issue that can be exploited since it uses DefaultServeMux which is accessible by all packages (even 3rd party)
 	// Instead use locally scoped servemux
@@ -35,6 +50,6 @@ func main() {
 	// http.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Listening on port :4000")
-	err := http.ListenAndServe(":4000", nil)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
