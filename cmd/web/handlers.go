29f1fb97057e577d93d4ca97f84e45187cbdcbbf
Pavle Djurdjic <66330623+kibonga@users.noreply.github.com>
2024-07-19 18:21:14 +0200

    added html templating (#4)
    Co-authored-by: Pavle Durdic <pavle.durdic.ext@infront.co>

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +14,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from quickbits!\n"))
+	// w.Write([]byte("Hello from quickbits!\n"))
+	files := []string{
+		"../../ui/html/home.page.tmpl",
+		"../../ui/html/footer.partial.tmpl",
+		"../../ui/html/base.layout.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
