ee040204d3266fd0e113f30e9de780c878fce0c1
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-07-19 13:14:12 +0200

    refactored servemux

--- main.go
+++ main.go
@@ -23,14 +23,16 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", home)
-	// mux.HandleFunc("/snippet/", showSnippet)
-	// mux.HandleFunc("/snippet/create", createSnippet)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	http.HandleFunc("/", home)
-	http.HandleFunc("/snippet/", showSnippet)
-	http.HandleFunc("/snippet/create", createSnippet)
+	// Potential security issue that can be exploited since it uses DefaultServeMux which is accessible by all packages (even 3rd party)
+	// Instead use locally scoped servemux
+	// http.HandleFunc("/", home)
+	// http.HandleFunc("/snippet/", showSnippet)
+	// http.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Listening on port :4000")
 	err := http.ListenAndServe(":4000", nil)
