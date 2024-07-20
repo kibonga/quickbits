b756c87ecbf98097e1f1fcb57c9582bcedb3664d
Pavle Djurdjic <66330623+kibonga@users.noreply.github.com>
2024-07-21 00:20:02 +0200

    created db conn pool and containarized go app and mysql server (#14)
    Co-authored-by: Pavle Durdic <pavle.durdic.ext@infront.co>

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,16 +2,20 @@ package main
 
 import "net/http"
 
-func (a *app) routes() {
-	a.mux.Handle("/handler", &myType{})
-	a.mux.HandleFunc("/handler/function", funcHandler)
+func (a *app) routes() *http.ServeMux {
+	mux := http.NewServeMux()
 
-	a.mux.Handle("/handler/func", http.HandlerFunc(funcHandler))
-	a.mux.HandleFunc("/", a.home)
-	a.mux.HandleFunc("/snippet", a.showSnippet)
-	a.mux.HandleFunc("/snippet/create", a.createSnippet)
+	mux.Handle("/handler", &myType{})
+	mux.HandleFunc("/handler/function", funcHandler)
 
-	a.mux.Handle("/static/", initFS())
+	mux.Handle("/handler/func", http.HandlerFunc(funcHandler))
+	mux.HandleFunc("/", a.home)
+	mux.HandleFunc("/snippet", a.showSnippet)
+	mux.HandleFunc("/snippet/create", a.createSnippet)
+
+	mux.Handle("/static/", initFS())
+
+	return mux
 }
 
 func initFS() http.Handler {
