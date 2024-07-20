c76591b94c2b7f6b33d45891f691bf7d75f99cb7
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-07-20 14:19:18 +0200

    added centralized logging

---
+++ cmd/web/helpers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+func (a *app) httpError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (a *app) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+
+	// a.errorLog.Fatal(trace)
+	a.errorLog.Output(2, trace)
+
+	a.httpError(w, http.StatusInternalServerError)
+}
+
+func (a *app) clientError(w http.ResponseWriter, status int) {
+	a.httpError(w, status)
+}
+
+func (a *app) notFound(w http.ResponseWriter) {
+	a.clientError(w, http.StatusNotFound)
+}
