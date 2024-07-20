61d3f5f3ed4d3cc2fd553f42929172ea80347159
Pavle Djurdjic <66330623+kibonga@users.noreply.github.com>
2024-07-20 14:25:40 +0200

    added routes package (#11)
    Co-authored-by: Pavle Durdic <pavle.durdic.ext@infront.co>

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,28 +32,9 @@ func (a *app) createServer() *http.Server {
 	}
 }
 
-func (a *app) initHandlers() {
-	a.mux.Handle("/handler", &myType{})
-	a.mux.HandleFunc("/handler/function", funcHandler)
-
-	a.mux.Handle("/handler/func", http.HandlerFunc(funcHandler))
-	a.mux.HandleFunc("/", a.home)
-	a.mux.HandleFunc("/snippet", a.showSnippet)
-	a.mux.HandleFunc("/snippet/create", a.createSnippet)
-
-	a.mux.Handle("/static/", initFS())
-}
-
-func initFS() http.Handler {
-	staticFileServer := http.FileServer(http.Dir("../../ui/static"))
-	staticFileHandler := http.StripPrefix("/static", staticFileServer)
-
-	return staticFileHandler
-}
-
 func main() {
 	app := initApp()
-	app.initHandlers()
+	app.routes()
 
 	srv := app.createServer()
 
