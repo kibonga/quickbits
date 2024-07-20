901ee7241e6a9b8489476eca8600f955266986e9
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-07-21 00:19:05 +0200

    created db conn pool and containarized go app and mysql server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type app struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
-	mux      *http.ServeMux
+}
+
+func main() {
+	app := initApp()
+
+	srv := app.createServer()
+	app.infoLog.Printf("address: %s", srv.Addr)
+
+	dsn := "web2:pass@tcp(quickbits_mysql:3306)/quickbits?parseTime=true"
+	db, err := connectDb(dsn)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app.infoLog.Printf("Starting server on %s", srv.Addr)
+	err = srv.ListenAndServe()
+	app.errorLog.Fatal(err)
 }
 
 func initApp() *app {
 	return &app{
 		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
-		mux:      http.NewServeMux(),
 	}
 }
 
@@ -28,17 +48,17 @@ func (a *app) createServer() *http.Server {
 	return &http.Server{
 		Addr:     *addr,
 		ErrorLog: a.errorLog,
-		Handler:  a.mux,
+		Handler:  a.routes(),
 	}
 }
 
-func main() {
-	app := initApp()
-	app.routes()
-
-	srv := app.createServer()
-
-	app.infoLog.Printf("Starting server on %s", srv.Addr)
-	err := srv.ListenAndServe()
-	app.errorLog.Fatal(err)
+func connectDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
