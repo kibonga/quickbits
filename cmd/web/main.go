c844b5c68f6619eb89cca5cf5d160bf8dd181732
Pavle Djurdjic <66330623+kibonga@users.noreply.github.com>
2024-07-20 11:57:10 +0200

    added leveled logging (#8)
    Co-authored-by: Pavle Durdic <pavle.durdic.ext@infront.co>

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.Handle("/handler", &myType{})
@@ -23,7 +27,13 @@ func main() {
 
 	mux.Handle("/static/", staticFileHandler)
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
