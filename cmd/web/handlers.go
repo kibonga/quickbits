193d8b72ce856db1c13fb7f670e0520437d896de
Pavle Durdic <pavle.durdic.ext@infront.co>
2024-07-25 10:47:01 +0200

    added html form validation and parsing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,13 +5,22 @@ import (
 	"errors"
 	"fmt"
 	"kibonga/quickbits/internal/models"
+	validator "kibonga/quickbits/internal/validators"
 	"net/http"
 	"strconv"
 
+	"github.com/go-playground/form/v4"
 	"github.com/julienschmidt/httprouter"
 )
 
 type myType struct{}
+
+type bitCreateForm struct {
+	Title               string `form:"title"`
+	Content             string `form:"content"`
+	ExpiresAt           int    `form:"expires"`
+	validator.Validator `form:"-"`
+}
 
 func (t *myType) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is my custom handler"))
@@ -34,7 +43,7 @@ func (a *app) bitsIndex(w http.ResponseWriter, r *http.Request) {
 	a.render(w, http.StatusOK, "home.tmpl", data)
 }
 
-func (a *app) bitsView(w http.ResponseWriter, r *http.Request) {
+func (a *app) bitView(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.Atoi(params.ByName("id"))
@@ -60,19 +69,57 @@ func (a *app) bitsView(w http.ResponseWriter, r *http.Request) {
 	a.render(w, http.StatusOK, "view.tmpl", data)
 }
 
-func (a *app) bitsCreate(w http.ResponseWriter, r *http.Request) {
-	id, err := a.bitModel.Insert("title", "content", 1)
+func (a *app) bitCreate(w http.ResponseWriter, r *http.Request) {
+	// bit := &models.InsertBit{}
+	// if err := json.NewDecoder(r.Body).Decode(&bit); err != nil {
+	// 	fmt.Println(err)
+	// 	a.clientError(w, http.StatusBadRequest)
+	// 	return
+	// }
+	// defer r.Body.Close()
+
+	form := &bitCreateForm{}
+	if err := a.decodePostForm(r, form); err != nil {
+		a.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "Title is required")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "Title must be less than 100 characters")
+	form.CheckField(validator.NotBlank(form.Content), "content", "Content is required")
+	form.CheckField(validator.MaxChars(form.Content, 1000), "content", "Content must be less than 1000 characters")
+	form.CheckField(validator.PermittedInt(form.ExpiresAt, 1, 7, 365), "expires", "Expires must be either 1, 7 or 365 days")
+
+	if !form.Valid() {
+		data := a.newTemplateData(r)
+		data.Form = form
+		a.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+
+	bit := &models.InsertBit{
+		Title:     form.Title,
+		Content:   form.Content,
+		DaysValid: form.ExpiresAt,
+	}
+	id, err := a.bitModel.Insert(bit)
+
 	if err != nil {
 		a.serverError(w, err)
 		return
 	}
 
 	a.infoLog.Printf("Inserted bit with ID %d\n", id)
-	http.Redirect(w, r, fmt.Sprintf("/bit/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/bits/view/%d", id), http.StatusSeeOther)
 }
 
-func (a *app) bitsNew(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display bit form for creating new bit"))
+func (a *app) bitCreateForm(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Display bit form for creating new bit"))
+	data := a.newTemplateData(r)
+	data.Form = &bitCreateForm{
+		ExpiresAt: 365,
+	}
+	a.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (a *app) updateBit(w http.ResponseWriter, r *http.Request) {
@@ -107,5 +154,23 @@ func (a *app) updateBit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	a.infoLog.Printf("updated bit with ID %d\n", id)
-	http.Redirect(w, r, fmt.Sprintf("/bits/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/bits/view/%d", id), http.StatusSeeOther)
+}
+
+func (a *app) decodePostForm(r *http.Request, dst any) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+
+	if err := a.formDecoder.Decode(dst, r.PostForm); err != nil {
+		var invalidDecoderError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+
+	return nil
 }
